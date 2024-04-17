aad02b6380ea31e7de49ece5f37ad6275a1fecd8
AFTECH.RO <38830718+aftechro@users.noreply.github.com>
2024-04-17 12:08:23 +0100

    Update .env.php
    DB config file. fixed the SSL connection

--- .env.php
+++ .env.php
@@ -5,17 +5,17 @@ define('DB_USER', 'booking');
 define('DB_PASSWORD', 'booking');
 define('DB_NAME', 'booking');
 
-// Establish database connection with error handling and SSL connection
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($conn, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, 3306, NULL, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
+// Establish database connection with error handling
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 // Function to escape user input for SQL query with additional checks
 function escapeString($conn, $value) {
-    if (is_object($conn) && property_exists($conn, 'server_info')) {
+    if ($conn && is_object($conn) && property_exists($conn, 'server_info')) {
         return mysqli_real_escape_string($conn, $value);
     } else {
         die("Invalid database connection");
